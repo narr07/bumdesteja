d0c405731fe1e57c6e7bed47333eede8db58ba5c
narr07 <dinarpermadi07@gmail.com>
2025-08-07 21:24:45 +0700

    feat: set revalidation time for Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import WisataSection from "@/components/WisataSection";
 import UmkmSection from "@/components/UmkmSection";
+export const revalidate = 60; 
 export default function Home() {
 	return (
 		<main className="scroll-container">
